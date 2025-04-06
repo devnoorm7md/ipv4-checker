1cbe25fbe652d23e28c35e74c40da4b85eb12277
Nour AbuFayyad <nourabufayyad@gmail.com>
2025-04-06 09:18:59 +0200

    add implementation for isValidIPv4 function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,5 +5,19 @@ fun main() {
 
 fun isValidIPv4(input: String):Boolean{
 
-    return false
+    if (input.contains(" ")) return false
+
+    val segments = input.split('.')
+
+    if (segments.size != 4) return false
+
+    for (segment in segments){
+        if (segment.isEmpty() || !segment.all { it.isDigit() }) return false
+        if (segment.length>1 && segment.startsWith('0')) return false
+        val convertedSegment = segment.toIntOrNull() ?: return false
+        if (convertedSegment !in 0 .. 255) return false
+
+    }
+
+    return true
 }
